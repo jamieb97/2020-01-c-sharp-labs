a841a971a9a3e801188c637658df491f5dbeb892
Jamie Benjamin <JBenjamin@spartaglobal.com>
2020-01-21 10:54:47 +0000

    OOP

---
+++ labs/lab_08_literal_objects/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+
+namespace lab_08_literal_objects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //array : fixed in sixe (Immutable)
+            var array01 = new int[5]; //empty memory space to hold 5 ints
+            var array02 = new int[] { 10, 20, 30, 40, 50 }; //literally defining numbers
+
+            var list01 = new List<int>();
+            var list02 = new List<int>() { 1, 2, 3, 4, 5 }; //literals
+
+            //random random
+            dynamic object01 = new ExpandoObject();
+            object01.name = "Fred";
+            object01.age = 22;
+            Console.WriteLine($"object01 has name {object01.name} and age {object01.age}");
+
+            //or
+            //creating object as literal
+            var object02 = new
+            {
+                name = "Fred",
+                age = 22
+            };
+            Console.WriteLine($"{object02.name} has age {object02.age}");
+
+            //oop language : create new object using literal data
+            var dog01 = new Dog() 
+            { 
+                name = "Milly",
+                age = 10
+            };
+            Console.WriteLine($"{dog01.name} is {dog01.age}");
+
+        }
+    }
+
+    class Dog 
+    {
+        public string name;
+        public int age;
+    }
+
+}
---
+++ labs/lab_09_OOP_intro/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace lab_09_OOP_intro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var keyword INFER TYPE FROM RIGHT ie Car
+            //car01 INSTANCE ie particular object created
+            //Car is template used
+            //() run a method when calling 'new' keyword // constructor
+            var car01 = new Car();
+            car01.Make = "Mclaren";
+            for(int i = 0; i < 1000; i++)
+            {
+                //CREATE 1000 CARS
+                var car = new Car();
+            }
+            var newCar = new Car();
+            Console.WriteLine($"Initial Speed {newCar.Speed}");
+            newCar.Speed++;
+            newCar.Speed++;
+            Console.WriteLine($"Final Speed{newCar.Speed}");
+            var car02 = new Car("Mclaren", "P1", "Orange", 2200, 200);
+        }
+    }
+
+
+
+
+    class Car
+    {
+        public string Make;
+        public string Model;
+        public string Colour;
+        public int Length;
+        public int Speed;
+
+        //Constructor is present even if not stated
+        //Default constructor
+        public Car() 
+        { 
+            this.Speed = 0; // THIS keyword refers to instance object in use at the time 
+        }
+
+        public Car(string make, string model, string colour, int length, int speed) 
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Colour = colour;
+            this.Length = length;
+            this.Speed = speed;
+        }
+    }
+}
