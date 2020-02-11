39a234ceb3d1931068c046b29564cb2a706c932e
Jamie-PC\Jamie <JB1650@student.anglia.ac.uk>
2020-02-11 22:22:03 +0000

    sql tests

---
+++ SQL/SQLNorthwindTest.sql
@@ -0,0 +1,12 @@
+﻿--1.1
+select CustomerID, CompanyName, Customers.Address, City from Customers where City = 'London' OR City = 'Paris' 
+--1.2
+select * from Products
+select * from Products where QuantityPerUnit LIKE '%bottle%'
+--1.3
+select ProductID, ProductName, Products.SupplierID, Suppliers.Country from Products
+JOIN Suppliers on Products.SupplierID = Suppliers.SupplierID
+--1.4
+select count(CategoryID), ProductName  from Products group by ProductName
+--1.5
+select CONCAT(CustomerID,', ', Country,', ', City) as yes from Customers
