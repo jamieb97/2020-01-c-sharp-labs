01f6cf686c93d1c6ae9aa23e39eca07f9a0c1ed6
jamie benjamin <j.benj167@gmail.com>
2020-02-11 16:01:01 +0000

    sql

---
+++ SQL/NorthwindTest.sql
@@ -0,0 +1,104 @@
+﻿select top 5 * from Customers
+select top 5 * from Customers order by CustomerID desc
+
+select * from Customers where City = 'London'
+
+--select * from INFORMATION_SCHEMA.COLUMNS where table_name = 'ContactTitle'
+select distinct TitleOfCourtesy from Employees
+select Count(distinct TitleOfCourtesy) from Employees
+
+select * from Employees where TitleOfCourtesy = 'Dr.'
+ 
+select Count(Discontinued) from Products where Discontinued = 'true'
+
+--offset = skip
+--first 5 customers
+select top (5) * from Customers
+--next 5 customers
+select * from Customers order by CustomerID offset 5 rows fetch next 5 rows only
+--next 5 customers
+select * from Customers order by CustomerID offset 10 rows fetch next 5 rows only
+
+--how many products with categoryid = 1 and discontinued
+select COUNT(*) from Products where CategoryID = '1' AND Discontinued = 'true'
+--how many products with items in stock and unitprice > 29.99
+select * from Products where UnitsInStock > 0 AND UnitPrice > 29.99 order by UnitPrice desc
+--how many distinct countries exists in customer tables
+select COUNT(distinct Country) from Customers
+select COUNT(distinct City) from Customers
+
+--LIKE
+--% is a wildcard for anything %a% contains a
+--                              a% starts with
+--                             %a  ends with
+
+--_ is a wildcard for ONE CHARACTER
+--how many products contain'ch'
+select * from Products where ProductName LIKE '%ch%'
+select COUNT(*) from Products where ProductName LIKE '%ch%'
+
+--how many regions contain the letter a in customers
+select COUNT(*) from Customers where Region LIKE '%a%'
+--how many regions start with a
+select COUNT(*) from Customers where Region LIKE 'a%'
+--how many regions end with
+select COUNT(*) from Customers where Region LIKE '%a'
+--NOT LIKE reverses the query
+select COUNT(*) from Customers where Region NOT LIKE '%a'
+--How many products to not contain ch
+select COUNT(*) from Products where ProductName NOT LIKE '%ch%'
+--using _ to represent single character wildcard
+--how many regions have 'A' as second letter 
+select Region, * from Customers where Region LIKE '_a%'
+--OR but for long lists use IN for a shorter version
+select Region, * from Customers where Region IN ('wa','ca')
+--BETWEEN NUMBERIC RANGES
+--how many products which are not discontinued have unitprice between 10.00 and 40.00
+select UnitPrice, * from Products where Discontinued = 0 and UnitPrice between 10.00 and 40.00
+--how many start with b,s or t
+select COUNT(*) from Products where ProductName LIKE ('B%') OR ProductName LIKE ('S%')
+
+select * from Products 
+join Categories
+on Products.CategoryID = Categories.CategoryID
+where Categories.CategoryName LIKE 'C%' OR CategoryName Like 'S%'
+
+--concatenation
+--select customers but join 'city and country' into one column eg 'lives in'
+select *, CONCAT (city, ', ', country) as livesin from Customers
+
+--customer => order => details of order (order_detail)
+--select orders for ALFKI customer
+select * from Orders where CustomerID = 'ALFKI'
+--have a look at order details as well
+select * from Orders
+join [Order Details] on Orders.OrderID = [Order Details].OrderID
+where CustomerID = 'alfki'
+
+--create a query to have oderid, productname and quantity
+select orderID, productName, Quantity from Products
+join [Order Details] on [Order Details].ProductID = Products.ProductID
+
+--filter by customer 'alkfi'
+select CustomerID, Orders.OrderID, ProductName, Quantity, o.UnitPrice from Orders
+join [Order Details] as o on Orders.OrderID = o.OrderID
+join Products on o.ProductID = Products.ProductID
+where CustomerID = 'alfki' order by UnitPrice desc
+
+--goal is to list, for any given customer, the order details
+--remember to get to the order details we have to go first through orders table 
+--  customer ==> order ==> order_details
+--                          productid bought, quantity, price and discount
+--note : customerid is already in orders table
+
+--get customer name and all orders for customerid = 'alfki'
+select Customers.CustomerID, ContactName, OrderID from Customers
+join Orders on Customers.CustomerID = Orders.CustomerID
+where Customers.CustomerID = 'alfki'
+
+--develop to show product id as well (include order details table)
+select Customers.CustomerID, ContactName, Products.ProductID, ProductName, orders.OrderID from Orders
+join [Order Details] as o on Orders.OrderID = o.OrderID
+join Customers on Orders.CustomerID = Customers.CustomerID
+join Products on o.ProductID = Products.ProductID
+where Customers.CustomerID = 'alfki'
---
+++ SQL/ToDoDatabase.sql
@@ -0,0 +1,58 @@
+﻿use master
+go 
+
+drop database if exists ToDoDatabase
+go
+
+create database ToDoDatabase
+go
+
+
+use ToDoDatabase
+go
+drop table if exists ToDoItems
+go
+drop table if exists Users
+go
+drop table if exists Categories
+go
+
+create table Users(
+	UserID INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+	UserName VARCHAR(50) NULL
+)
+create table Categories(
+	CategoryID INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+	CategoryName VARCHAR(50) NULL
+)
+
+create table ToDoItems(
+	ToDoItemID INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+	ToDoItemName VARCHAR(50) NULL,
+	StartDate DATE NULL,
+	DONE BIT NULL,
+	UserID INT NULL FOREIGN KEY REFERENCES Users(UserID),
+	CategoryID INT NULL FOREIGN KEY REFERENCES Categories(CategoryID)
+)
+
+INSERT INTO Users VALUES ('Wilder')
+INSERT INTO Users VALUES ('Fury')
+INSERT INTO Users VALUES ('Joshua')
+
+INSERT INTO Categories VALUES ('Pow')
+INSERT INTO Categories VALUES ('Thump')
+INSERT INTO Categories VALUES ('Yeet')
+
+INSERT INTO ToDoItems VALUES ('idk', '2020-01-23', 'true', 1, 1)
+INSERT INTO ToDoItems VALUES ('maybe', '2020-01-23', 'true', 2, 2)
+INSERT INTO ToDoItems VALUES ('nope', '2020-01-23', 'true', 3, 2)
+
+ALTER TABLE Users ADD UserAge VARCHAR(50) NULL
+ALTER TABLE Users ALTER COLUMN UserAge INT NULL
+UPDATE Users SET UserAge = 22 WHERE UserID = 1  
+
+select ToDoItemID, ToDoItemName, CategoryName, UserName, StartDate, case when DONE = 1 then 'True' else 'false' end as Completed, UserAge from ToDoItems
+inner join Categories on ToDoItems.CategoryID = Categories.CategoryID
+inner join Users on ToDoItems.UserID = Users.UserID
+
+select * from INFORMATION_SCHEMA.COLUMNS
