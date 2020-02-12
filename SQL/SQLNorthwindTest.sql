f1943cded2b2ef2dc18f561304345cce8ba4fd22
Jamie-PC\Jamie <JB1650@student.anglia.ac.uk>
2020-02-12 22:14:26 +0000

    yep

--- SQL/SQLNorthwindTest.sql
+++ SQL/SQLNorthwindTest.sql
@@ -14,9 +14,29 @@ GROUP BY c.CategoryName
 --1.5
 select CONCAT(TitleOfCourtesy, FirstName,' ', LastName) as yes from Employees e
 --1.6
-
+SELECT ROUND(COUNT(od.Quantity),2) AS [TOTAL SALES] FROM [Order Details] OD
+JOIN Orders o ON OD.OrderID = o.OrderID
+JOIN EmployeeTerritories et ON o.EmployeeID = et.EmployeeID
+JOIN Territories t ON et.TerritoryID = t.TerritoryID
+JOIN Region r ON t.RegionID = r.RegionID
+where OD.Quantity > 1000000
 --1.7
 SELECT COUNT(*) FROM Orders WHERE Freight > 100.00 AND ShipCountry in ('UK', 'USA')
 --1.8
 select od.OrderID, SUM((od.UnitPrice*od.Quantity) * (1-od.Discount)) as 'Yes' from [Order Details] od
 group by od.OrderID
+
+
+--CREATE TABLE Spartans(
+--	SpartanID INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+--	SpartanName VARCHAR NULL,
+--	SpartanUni VARCHAR NULL,
+--	SpartanCourse VARCHAR NULL,
+--	SpartanGrade INT NULL
+--)
+
+--INSERT INTO Spartans VALUES ('Jamie', 'Anglia Ruskin', 'Computer Science', 2)
+
+--SELECT * FROM Spartans
+
+DROP TABLE Spartans
