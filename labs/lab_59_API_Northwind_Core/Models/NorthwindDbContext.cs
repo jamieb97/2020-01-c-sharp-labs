9017146d76b034ea737ed2457f3614885dc412bc
jamie benjamin <j.benj167@gmail.com>
2020-02-25 16:33:05 +0000

    nd

---
+++ labs/lab_59_API_Northwind_Core/Models/NorthwindDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace lab_59_API_Northwind_Core
+{
+    public class NorthwindDbContext : DbContext
+    {
+        public NorthwindDbContext(DbContextOptions<NorthwindDbContext> options) : base(options){}
+
+        //connection string
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            builder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Northwind;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+        //DbSet customer
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
